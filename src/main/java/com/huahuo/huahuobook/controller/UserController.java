ce12a36c3b6198c554c4bb391cb02819c840db8b
Huahuoao <2294198058@qq.com>
2023-01-23 23:55:29 +0800

    项目搭建，登录注册

--- src/main/java/com/huahuo/huahuobook/controller/UserController.java
+++ src/main/java/com/huahuo/huahuobook/controller/UserController.java
@@ -6,7 +6,6 @@ import com.huahuo.huahuobook.pojo.User;
 import com.huahuo.huahuobook.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
